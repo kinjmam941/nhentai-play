50151e3c088659edf9451ddc117c276c01b143cb
kinjmam941 <152094507+kinjmam941@users.noreply.github.com>
2025-04-29 16:22:53 +0530

    Update home.py

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,15 +1,20 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-from app.utils.client import hhaven_client
+from app.utils.client import client
 
 router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    data = await hhaven_client.home()
-    return request.app.state.templates.TemplateResponse("home.html", {
-        "request": request,
-        "trending": data.trending_month,
-        "latest": data.last,
-        "genres": data.yuri + data.ecchi + data.uncensored
-    })
+    homepage = await client.home()
+    genres = await client.get_all_genres()  # Fixed genre loading
+    
+    return request.app.state.templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "trending": homepage.trending_month,
+            "latest": homepage.last,
+            "genres": genres[:8]  # Show first 8 genres
+        }
+    )
